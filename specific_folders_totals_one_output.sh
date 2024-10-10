c514a028c2b4892fbca398c38e4784477f57d774
Cuauhtemoc Guerrero <ccguerrero@gmail.com>
2024-10-10 15:54:01 -0700

    Refactor specific_folders_totals_one_output.sh script for better readability and maintainability

--- specific_folders_totals_one_output.sh
+++ specific_folders_totals_one_output.sh
@@ -25,7 +25,7 @@ if [ -z "$1" ]; then
 fi
 
 source_file="$1"
-output_file="debug.csv"
+output_file="folders_output.csv"
 
 # Extract the number after 's' in the source file name
 school_number=$(echo "$source_file" | grep -oP '(?<=s)\d{2}')
@@ -51,7 +51,8 @@ folders=("Videos" "Pictures" "Music" "Downloads" "Documents" "Desktop")
 # Read the file and sum up the sizes in bytes for each specified folder
 while IFS= read -r line; do
     if [[ $line =~ ^d-r--- ]]; then
-        size=$(echo $line | grep -oP '([0-9]+(\.[0-9]+)?)')
+        #size=$(echo $line | grep -oP '([0-9]+(\.[0-9]+)?)')
+        size=$(echo $line | awk '{print $2}')
         unit=$(echo $line | grep -oP '([KMGT]B|B)')
 
         case $unit in
@@ -62,13 +63,20 @@ while IFS= read -r line; do
             TB) size=$(echo "$size * 1024 * 1024 * 1024 * 1024" | bc) ;;
         esac
 
+        found=false
         for folder in "${folders[@]}"; do
             if [[ $line =~ (^|[^a-zA-Z0-9])$folder([^a-zA-Z0-9]|$) ]]; then
                 folder_size[$folder]+="$size "
-		        folder_unit[$folder]+="$unit "
+                folder_unit[$folder]+="$unit "
                 folder_totals[$folder]=$(echo "${folder_totals[$folder]:-0} + $size" | bc)
+                found=true
+                break
             fi
         done
+
+        if [[ $found == "false" && $line != *"│   │   │   ├──"* ]]; then
+            other_totals=$(echo "${other_totals:-0} + $size" | bc)
+        fi
     fi
 done < "$source_file"
 
@@ -84,20 +92,20 @@ for folder in "${folders[@]}"; do
     esac
 done
 
-printf "Pics: %s B\n" "$pics_totals"
-printf "Music: %s B\n" "$music_totals"
+#printf "Pics: %s B\n" "$pics_totals"
+#printf "Music: %s B\n" "$music_totals"
 
 # Write the header only if the output file doesn't exist
 if [ ! -f "$output_file" ]; then
-    echo "Location,Videos (B), Pictures (B),Music (B),Downloads (B),Documents (B),Desktop (B),Total GB" > "$output_file"
+    echo "Location,Videos (B), Pictures (B),Music (B),Downloads (B),Documents (B),Desktop (B),Others (B),Total GB" > "$output_file"
 fi
 
 # Calculate the total size in GB
 total=0
-total=$(echo "scale=2; ${video_totals}+${pics_totals}+${music_totals}+${down_totals}+${docs_totals}+${desk_totals}" | bc)
+total=$(echo "scale=2; ${video_totals}+${pics_totals}+${music_totals}+${down_totals}+${docs_totals}+${desk_totals}+${other_totals}" | bc)
 total=$(echo "scale=2; ${total}/(1024*1024*1024)" | bc)
 
 # Output the result to a CSV file
-echo "$location,$video_totals,$pics_totals,$music_totals,$down_totals,$docs_totals,$desk_totals,$total" >> "$output_file"
+echo "$location,$video_totals,$pics_totals,$music_totals,$down_totals,$docs_totals,$desk_totals,$other_totals,$total" >> "$output_file"
 
 echo "Results have been written to $output_file"
